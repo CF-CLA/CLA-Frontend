951dc17dd7656c7279dca789ad3515b1edd5dc74
Keian <KeianAnthony1@gmail.com>
2022-01-21 15:54:46 -0800

    adds basic structure, waiting for back-end to be functional

---
+++ src/app.js
@@ -0,0 +1,64 @@
+require('dotenv').config();
+const readline = require('readline');
+const rl = readline.createInterface(process.stdin, process.stdout);
+const bcrypt = require('bcrypt');
+const axios = require('axios');
+
+let answerData = {
+  username: '',
+  password: '',
+  returnData: {},
+}
+
+async function signInUser(username, password) {
+  let requestConfig = {
+    method: 'get',
+    baseURL: process.env.BASE_URL,
+    url: '/getUserData',
+    auth: {
+      username: username,
+      password: password
+    }
+  }
+  answerData.returnData = await axios(requestConfig);
+}
+
+async function signUpUser(username, password) {
+  let requestConfig = {
+    method: 'post',
+    baseURL: process.env.BASE_URL,
+    url: '/createUser',
+    auth: {
+      username: username,
+      password: password
+    }
+  }
+  answerData.returnData = await axios(requestConfig);
+}
+
+rl.question("Do you have an account? (please enter 'yes' or 'no') ", function (createdAct) {
+  let createdActStr = createdAct.toLowerCase().trim();
+  if (createdActStr === 'yes') {
+    // Get credentials for logging in existing user
+    rl.question("What is your username? ", function (username) {
+      answerData.username = username;
+      rl.question("What is your password? ", async function (password) {
+        answerData.password = await bcrypt.hash(password, 10);
+        // Axios request receiving user data
+        signInUser();
+      });
+    });
+  } else {
+    // Get credentials to make an account
+    rl.question("What is your username? ", function (username) {
+      answerData.username = username;
+      rl.question("What is your password? ", async function (password) {
+        answerData.password = await bcrypt.hash(password, 10);
+        // Axios request making account
+        signUpUser();
+      });
+    });
+  }
+});
+
+console.log(answerData.returnData);
